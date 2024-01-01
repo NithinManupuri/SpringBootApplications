698dc99e84beab54cfe24da815a8828cd0446263
NithinManupuri <manupurinithin@gmail.com>
2024-01-01 16:11:11 +0530

    deleted

--- Assignment-01/src/main/java/springdatajpa/dao/DonorRepository.java
+++
@@ -1,9 +0,0 @@
-package springdatajpa.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import springdatajpa.entitiy.Donors;
-
-public interface DonorRepository  extends JpaRepository<Donors,String>{
-
-}
